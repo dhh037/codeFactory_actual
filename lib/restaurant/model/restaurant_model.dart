1a847bffb19a917492d351066317e4c0deb9a5cf
dh037 <donghyeok.k@dvmon.com>
2022-11-01 22:57:19 +0900

    fromModel 생성자 만들어보기

--- lib/restaurant/model/restaurant_model.dart
+++ lib/restaurant/model/restaurant_model.dart
@@ -1,8 +1,6 @@
-enum RestaurantPriceRange {
-  expensive,
-  medium,
-  cheap
-}
+import '../../common/const/data.dart';
+
+enum RestaurantPriceRange { expensive, medium, cheap }
 
 class RestaurantModel {
   final String id;
@@ -10,20 +8,33 @@ class RestaurantModel {
   final String thumbUrl;
   final List<String> tags;
   final RestaurantPriceRange priceRange;
-  final double rations;
+  final double ratings;
   final int ratingsCount;
   final int deliveryTime;
   final int deliveryFee;
 
-  RestaurantModel({
-    required this.id,
-    required this.name,
-    required this.thumbUrl,
-    required this.tags,
-    required this.priceRange,
-    required this.rations,
-    required this.ratingsCount,
-    required this.deliveryTime,
-    required this.deliveryFee
-  });
+  RestaurantModel(
+      {required this.id,
+      required this.name,
+      required this.thumbUrl,
+      required this.tags,
+      required this.priceRange,
+      required this.ratings,
+      required this.ratingsCount,
+      required this.deliveryTime,
+      required this.deliveryFee});
+
+  factory RestaurantModel.fromJson({required Map<String, dynamic> json}) {
+    return RestaurantModel(
+        id: json['id'],
+        name: json['name'],
+        thumbUrl: 'http://$ip${json['thumbUrl']}',
+        tags: List<String>.from(json['tags']),
+        priceRange: RestaurantPriceRange.values
+            .firstWhere((e) => e.name == json['priceRange']),
+        ratings: json['ratings'],
+        ratingsCount: json['ratingsCount'],
+        deliveryTime: json['deliveryTime'],
+        deliveryFee: json['deliveryFee']);
+  }
 }
--- lib/restaurant/view/restaurant_screen.dart
+++ lib/restaurant/view/restaurant_screen.dart
@@ -36,31 +36,9 @@ class RestaurantScreen extends StatelessWidget {
                       itemBuilder: (context, index) {
                         final item = snapshot.data![index];
                         // parsed
-                        final pItem = RestaurantModel(
-                            id: item['id'],
-                            name: item['name'],
-                            thumbUrl: 'http://$ip${item['thumbUrl']}',
-                            tags: List<String>.from(item['tags']),
-                            priceRange: RestaurantPriceRange.values.firstWhere(
-                                (e) => e.name == item['priceRange']),
-                            rations: item['ratings'],
-                            ratingsCount: item['ratingsCount'],
-                            deliveryTime: item['deliveryTime'],
-                            deliveryFee: item['deliveryFee']);
+                        final pItem = RestaurantModel.fromJson(json: item);
 
-                        return RestaurantCard(
-                            image: Image.network(pItem.thumbUrl,
-                                fit: BoxFit.cover),
-                            // image: Image.asset(
-                            //     'asset/img/food/ddeok_bok_gi.jpg',
-                            //     fit: BoxFit.cover),
-                            name: pItem.name,
-                            // List<dynamic>을 List<String>으로 바꾸는 기술
-                            tags: pItem.tags,
-                            ratingsCount: pItem.ratingsCount,
-                            deliveryTime: pItem.deliveryTime,
-                            deliveryFee: pItem.deliveryFee,
-                            ratings: pItem.rations);
+                        return RestaurantCard.fromModel(model: pItem);
                       },
                       separatorBuilder: (context, index) {
                         return SizedBox(height: 16.0);
