6cc86aa52ca3b612d92229a1bd00dc8819a95509
dh037 <donghyeok.k@dvmon.com>
2022-11-01 23:29:05 +0900

    ProductCard 작업하기

---
+++ lib/product/component/product_card.dart
@@ -0,0 +1,54 @@
+import 'package:actual/common/const/colors.dart';
+import 'package:flutter/material.dart';
+
+class ProductCard extends StatelessWidget {
+  const ProductCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return IntrinsicHeight(
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(8.0),
+            child: Image.asset(
+              'asset/img/food/ddeok_bok_gi.jpg',
+              width: 110,
+              height: 100,
+              fit: BoxFit.cover,
+            ),
+          ),
+          const SizedBox(width: 16.0),
+          Expanded(
+              child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "떡볶이",
+                style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 18.0,
+                ),
+              ),
+              Text(
+                "정통 떡볶이의 정석!\n맛있습니다.",
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+                style: TextStyle(color: BODY_TEXT_COLOR, fontSize: 14.0),
+              ),
+              Text(
+                "₩10000",
+                textAlign: TextAlign.right,
+                style: TextStyle(
+                    color: PRIMARY_COLOR,
+                    fontSize: 12.0,
+                    fontWeight: FontWeight.w500),
+              )
+            ],
+          ))
+        ],
+      ),
+    );
+  }
+}
